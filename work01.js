0444989a361d87e921b44b421df570c68553b633
qlalfdmlghk1 <121783811+qlalfdmlghk1@users.noreply.github.com>
2023-08-22 00:35:08 +0900

    Add files via upload

---
+++ work01.js
@@ -0,0 +1,14 @@
+const arrowMinus = (p1,p2) =>{
+    return p1 - p2;
+}
+console.log(arrowMinus(3,9));
+
+const arrowMul = (p1,p2) =>{
+    return p1 * p2;
+}
+console.log(arrowMul(3,9));
+
+const arrowDiv = (p1,p2) =>{
+    return p1 / p2;
+}
+console.log(arrowDiv(3,9));
